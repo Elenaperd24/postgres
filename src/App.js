3f0d3ef7c479e599017d1dc00a40e68e22230ead
Elenaperd24 <elena24.perdomo@gmail.com>
2023-11-03 13:49:25 -0300

    psql

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const addUser = async (e) => {
+    e.preventDefault();
+    const data = {
+      name: e.target[0].value,
+      email: e.target[1].value,
+    };
+    const response = await axios.post("http://localhost:4000/newuser", data);
+
+    console.log(response.data);
+  };
+
+  const showData = async (e) => {
+    e.preventDefault();
+    const data = {
+      name: e.target[0].value,
+      email: e.target[1].value,
+    };
+    const response = await axios.post("http://localhost:4000/newuser", data);
+
+    console.log(response.data);
+  };
+
+  const getDatabyId = async (e) => {
+    e.preventDefault();
+    const data = {
+      name: e.target[0].value,
+      email: e.target[1].value,
+    };
+    const response = await axios.post("http://localhost:4000/newuser", data);
+
+    console.log(response.data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="App">
+        <form
+          style={{ display: "flex", flexDirection: "column", width: "80%" }}
+          onSubmit={addUser}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <input type="name" placeholder="name" />
+          <input type="email" placeholder="email" />
+          <button type="submit">ADD USER</button>
+        </form>
+      </div>
+      <br/>
+
+{/* show data */}
+      <div className="App">
+        <form
+          style={{ display: "flex", flexDirection: "column", width: "80%" }}
+          onSubmit={showData}
+        >
+          <button type="submit">SHOW DATA</button>
+        </form>
+      </div>
+      <br/>
+{/* show data by id */}
+      <div className="App">
+        <form
+          style={{ display: "flex", flexDirection: "column", width: "80%" }}
+          onSubmit={getDatabyId}
+        >
+          <input type="number" placeholder="id" />
+          <button type="submit">SHOW id User</button>
+        </form>
+      </div>
+    </>
   );
 }
 
